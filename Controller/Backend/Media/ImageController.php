6598aa948085199cd35f2096c8ae562373468a65
yanickouellet <yanickouellet555@gmail.com>
2013-06-26 16:14:31 -0400

    Cleanup

--- Controller/Backend/Media/ImageController.php
+++ Controller/Backend/Media/ImageController.php
@@ -38,35 +38,7 @@ class ImageController extends BaseController
 		$this->mediaRepository = $this->mediaRepository = $this->getEm()->getRepository('EgzaktMediaBundle:Media');
     }
 
-	public function openPixlrAction($id)
-	{
-		/** @var Image $image */
-		$image = $this->mediaRepository->find($id);
-		if(!$image){
-			return $this->redirect($this->generateUrl('egzakt_media_backend_media'));
-		}
-
-		$parameters = array(
-			'referrer' => "Egzakt Media Bundle",
-			'image' => 'http://staging.egz-med.p.egzakt.com/'.$image->getMediaPath(),
-			'exit' => 'http://staging.egz-med.p.egzakt.com/'.$image->getRouteBackend('edit'), $image->getRouteBackendParams(),
-			'title' => $image->getName(),
-			'method' => "GET",
-			'target' => 'http://staging.egz-med.p.egzakt.com/'.$this->generateUrl($image->getRouteBackend('pixlr_edit'), $image->getRouteBackendParams()),
-		);
-
-		$query = array();
-		foreach($parameters as $key => $value)
-		{
-			$query[] = sprintf("%s=%s", $key, $value);
-		}
-
-		$url = "http://	pixlr.com/express/?".implode('&', $query);
-
-		return $this->redirect($url);
-	}
-
-	public function editPixrlAction($id, Request $request)
+	public function editImageAction($id, Request $request)
 	{
 		/** @var Image $image */
 		$image = $this->mediaRepository->find($id);
@@ -77,13 +49,10 @@ class ImageController extends BaseController
 		$path = $this->get('kernel')->getRootDir().'/../web'.$image->getMediaPath();
 		file_put_contents($path, file_get_contents($request->get('image')));
 
-		$image->setName($request->get('title'));
 		$this->getEm()->persist($image);
 		$this->getEm()->flush();
 
-		return new JsonResponse(json_encode(array(
-			"src" => $this->container->get('templating.helper.assets')->getUrl($image->getMediaPath()),
-		)));
+		return new JsonResponse(json_encode(array()));
 	}
 
 }
