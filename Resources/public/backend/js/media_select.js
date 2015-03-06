ca771ee744bc082940171c57e1fb57a4a2af3175
Jean-François Côté <tioiscote@gmail.com>
2015-03-06 12:26:49 -0500

    Bring back the autoUpload when accessing the MediaManager from an entity or CKEditor

--- Resources/public/backend/js/media_select.js
+++ Resources/public/backend/js/media_select.js
@@ -462,10 +462,40 @@ var mediaManagerBind = function () {
     // JQUERY UPLOADER
 
     // Initialize the jQuery File Upload widget:
+    var fileCount = 0;
+    var successes = 0;
+    var fails = 0;
+
+    // Initialize the jQuery File Upload widget:
     $('#fileupload').fileupload({
-        // Uncomment the following to send cross-domain cookies:
-        //xhrFields: {withCredentials: true},
+        autoUpload: true,
         url: Routing.generate('unifik_media_backend_media_upload')
+    }).bind('fileuploaddone', function(e, data) {
+        fileCount++;
+        successes++;
+        //console.log('fileuploaddone');
+        if (fileCount === data.getNumberOfFiles()) {
+            //console.log('all done, successes: ' + successes + ', fails: ' + fails);
+
+            fileCount = 0;
+            successes = 0;
+            fails = 0;
+
+            $('a.library').click();
+        }
+    }).bind('fileuploadfail', function(e, data) {
+        fileCount++;
+        fails++;
+        //console.log('fileuploadfail');
+        if (fileCount === data.getNumberOfFiles()) {
+            //console.log('all done, successes: ' + successes + ', fails: ' + fails);
+
+            fileCount = 0;
+            successes = 0;
+            fails = 0;
+
+            $('a.library').click();
+        }
     });
 
     // EMBED VIDEO ADD SCRIPT
